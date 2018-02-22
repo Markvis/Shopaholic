5eded579128e6d7605fd30f05d486b37d1c5146c
Mark Favis <favismark@gmail.com>
2018-02-22 01:16:22 -0800

    updated pom.xml

--- FrontendParserJava/src/main/java/com/favis/shopaholic/Runner.java
+++ FrontendParserJava/src/main/java/com/favis/shopaholic/Runner.java
@@ -3,6 +3,6 @@ package com.favis.shopaholic;
 public class Runner {
 
     public static void main(String... args){
-        System.out.println("hi");
+        System.out.println("Hi This is mark");
     }
 }
